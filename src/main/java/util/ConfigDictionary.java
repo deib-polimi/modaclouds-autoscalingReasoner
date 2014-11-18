4ab08fee001c25c52f49c88a67e60671c2a8ecea
MicheleGuerriero <michele.guerriero.2391@gmail.com>
2014-11-18 18:40:56 +0100

    Added ssh connector implementation for communicate with the serve hosting the capacity allocation algorithm.

---
+++ src/main/java/util/ConfigDictionary.java
@@ -0,0 +1,18 @@
+package util;
+
+public class ConfigDictionary {
+
+	public static final String pathToPCMResourceEnvironment = "pathToPCMResourceEnvironment";
+	public static final String pathToPCMAllocation = "pathToPCMAllocation";
+	public static final String pathToSPCMystem = "pathToSPCMystem";
+	public static final String pathToS4CResourceModelExtension = "pathToS4CResourceModelExtension";
+	public static final String pathToLineResult = "pathToLineResult";
+	public static final String speedNorm = "speedNorm";
+	public static final String SSH_USER_NAME="SSH_USER_NAME";
+	public static final String SSH_HOST="SSH_HOST";
+	public static final String SSH_PASSWORD="SSH_PASSWORD";
+	public static final String RUN_WORKING_DIRECTORY="RUN_WORKING_DIRECTORY";
+	public static final String DEFAULT_BASH="DEFAULT_BASH";
+	public static final String INPUT_FOLDER="INPUT_FOLDER";
+
+}
